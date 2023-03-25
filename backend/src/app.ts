9f81e94df07ba604b2e8326d2a9a0638d9fae5af
RichieBrady <richard.brady@ucdconnect.ie>
2023-03-25 13:46:08 +0000

    Add logging and error handling middleware.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,7 +11,9 @@ const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
 
+import {errorHandler} from "./middleware/errorHandler";
 import indexRouter from './routes/index.js';
+
 
 const app = express();
 
@@ -28,8 +30,22 @@ app.use(cors());
 app.use('/', indexRouter);
 
 
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
 
-// catch 404 and forward to error handle
+// error handler
+app.use(function (err: { message: any; status: any; }, req: { app: { get: (arg0: string) => string; }; }, res: { locals: { message: any; error: any; }; status: (arg0: any) => void; render: (arg0: string) => void; }, next: any) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
 app.all('*', (req, res) => {
     res.status(404)
     if (req.accepts('html')) {
@@ -40,6 +56,8 @@ app.all('*', (req, res) => {
         res.type('txt').send('404 Not Found')
     }
 })
+
+app.use(errorHandler)
 
 export default app
 
