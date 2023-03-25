e34701db7d13d2de0d65c117b248cc80274088ae
RichieBrady <richard.brady@ucdconnect.ie>
2023-03-25 15:25:42 +0000

    Add mongoDB db connection (requires .env file with DATABASE_URI) and basic user model. Log DB errors to db log file.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,14 @@
-import mongoose from "mongoose";
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
 import cors from 'cors';
 import {corsOptions} from "./config/corsOptions.js";
+import {connectDB} from "./config/databaseConfig.js";
+import mongoose from "mongoose";
 import express from "express";
 import path from "path";
 import logger from 'morgan';
-import createError from 'http-errors';
 import cookieParser from 'cookie-parser';
 import {fileURLToPath} from 'url';
 
@@ -12,9 +16,10 @@ const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
 
+connectDB()
+
 import {errorHandler} from "./middleware/errorHandler.js";
 import indexRouter from './routes/index.js';
-
 
 const app = express();
 
@@ -29,23 +34,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors(corsOptions));
 
 app.use('/', indexRouter);
-
-
-// catch 404 and forward to error handler
-//app.use(function (req, res, next) {
-//    next(createError(404));
-//});
-//
-//// error handler
-//app.use(function (err: { message: any; status: any; }, req: { app: { get: (arg0: string) => string; }; }, res: { locals: { message: any; error: any; }; status: (arg0: any) => void; render: (arg0: string) => void; }, next: any) {
-//    // set locals, only providing error in development
-//    res.locals.message = err.message;
-//    res.locals.error = req.app.get('env') === 'development' ? err : {};
-//
-//    // render the error page
-//    res.status(err.status || 500);
-//    res.render('error');
-//});
 
 app.all('*', (req, res) => {
     res.status(404)
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,16 @@
 import app from "./app.js";
+import mongoose from "mongoose";
+import {logEvents} from "./middleware/logger.js";
 
-app.listen(3000, () => {
-    console.log("Server is running on port " + 3000);
-});
+mongoose.connection.once('open', () => {
+    console.log('Connected to DB')
+    app.listen(3000, () => {
+        console.log("Server is running on port " + 3000);
+    });
+})
+
+mongoose.connection.on('error', err => {
+    console.log(err)
+    logEvents(`${err.no}\t${err.code}\t${err.syscall}\t${err.hostname}`, 'dbErrorLog.log')
+})
 
